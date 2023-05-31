627027421517a8d194dfdc1c30ebe8cbd22bbe36
liuzuwu <zuwu.liu@cnydsimtek.com>
2023-05-31 09:16:17 +0800

    撤销

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/liuzuwu9420/liuzuwu9420.github.io.git master:deploy
+git push -f https://github.com/liuzuwu9420/liuzuwu9420.github.io.git master:gh-pages
 
 cd -
